0d180ca464cf9481295c19b353a401cb9dccdccd
SINtyanneru <irjitgd@gmail.com>
2025-06-13 16:22:34 +0900

    ホーム画面に一覧を載せてみた

--- src/Page/home.php
+++ src/Page/home.php
@@ -1,1 +1,29 @@
-<?=$CONFIG["PAGE"]["NAME"]?>へようこそ！
+<?=$CONFIG["PAGE"]["NAME"]?>へようこそ！<BR>
+
+<BR>
+
+最新の記事<BR>
+<?php
+$GetLatestPageSQL = SQL_RUN($PDO,
+	<<<TEXT
+		SELECT
+			`D`.`PAGE` AS `ID`,
+			`D`.`TITLE`,
+			`D`.`DATE`
+		FROM
+			`PAGE_INFO` AS `I`
+		JOIN
+			`PAGE_DATA` AS `D` ON `I`.`ID` = `D`.`PAGE`
+		ORDER
+			BY `D`.`DATE` DESC
+		LIMIT 1;
+	TEXT,
+	[]
+);
+
+if ($GetLatestPageSQL["STATUS"]) {
+	foreach ($GetLatestPageSQL["RESULT"] as $Row) {
+		echo "<A HREF=\"/page/".htmlspecialchars($Row["TITLE"])."\">".htmlspecialchars($Row["TITLE"])."</A>";
+	}
+}
+?>
